3d667b12572868cba6471038766075cccb785791
Chris Travers <chris@efficito.com>
2014-04-21 01:48:19 -0700

    Adding readme

--- pg_monitoring--0.1.sql
+++ pg_monitoring--0.1.sql
@@ -1,13 +1,16 @@
-\echo Use "CREATE EXTENSION pg_monitoring" to load this file. \quit
+-- \echo Use "CREATE EXTENSION pg_monitoring" to load this file. \quit
 
 
--- As of Pg 9.2 at least, the pg_stat_replication view joins together several functions, not directly hitting relations
--- Wrapping this in a function thus poses no performance issues currently.  If this ever changes though, we may want to 
--- change our approach here.
+CREATE TABLE pg_monitoring_last_run (
+     proname name,
+     runtime timestamp
+);
+
+-- this is a snapshot so no need for last run handling
 
 CREATE OR REPLACE FUNCTION pg_monitoring_lag_info()
 RETURNS TABLE (
-            usename text,
+            usename name,
    application_name text,
     client_hostname text, 
         client_addr inet,
@@ -16,15 +19,17 @@ RETURNS TABLE (
               state text,
       sync_priority int,
          sync_state text,
-          total_lag numeric
+          total_lag double precision
 )
 LANGUAGE SQL AS
 $$
-SELECT client_hostname, client_addr,
+SELECT usename, application_name, client_hostname, client_addr, client_port,
+       backend_start, state, sync_priority, sync_state,
        ((cur_xlog * 255 * 16 ^ 6) + cur_offset) 
-       - ((replay_xlog * 255 * 16 ^ 6) + replay_offset) as total_lag,
+       - ((replay_xlog * 255 * 16 ^ 6) + replay_offset) as total_lag
 FROM (
-      SELECT client_hostname, client_addr,
+      SELECT usename, application_name, client_hostname, client_addr, 
+             client_port, backend_start, state, sync_priority, sync_state,
              ('x' || lpad(split_part(replay_location, '/', 1), 8, '0')
              )::bit(32)::bigint AS replay_xlog,
              ('x' || lpad(split_part(replay_location, '/', 2), 8, '0')
@@ -41,10 +46,11 @@ $$;
 
 
 CREATE OR REPLACE FUNCTION pg_monitoring_time_since_replay()
-RETURNS int LANGUAGE SQL AS
+RETURNS double precision
+LANGUAGE SQL AS
 $$ SELECT extract(epoch from now() - pg_last_xact_replay_timestamp()); $$;
 
-COMMENT ON function pg_monitoring_replication_lag_info() IS
+COMMENT ON function pg_monitoring_lag_info() IS
 $$This is a simple function to grab lag info in bytes.  This must be a 
 function since it requires superuser privileges to run and we do not want to
 run the monitoring as superuser.$$;
@@ -52,46 +58,68 @@ run the monitoring as superuser.$$;
 COMMENT ON FUNCTION pg_monitoring_time_since_replay()
 IS $$ Simple function to grab time from last write on slave.  $$; 
 
+CREATE TABLE pg_monitoring_table_load_lastvals (
+    rows_select_idx numeric,
+    rows_select_scan numeric,
+    rows_insert numeric,
+    rows_update numeric,
+    rows_delete numeric,
+    rows_total numeric
+);
+
+insert into pg_monitoring_table_load_lastvals (rows_total) values (null);
+
 CREATE OR REPLACE FUNCTION pg_monitoring_load_across_tables()
-RETURNS TABLE (
-    rows_select_idx bigint,
-    rows_select_scan bigint,
-    rows_insert bigint,
-    rows_update bigint,
-    rows_delete bigint,
-    rows_total bigint
-)
+RETURNS SETOF pg_monitoring_table_load_lastvals
 LANGUAGE sql AS
 $$
 -- Copied and pasted from the MIT Licensed ScoutApp Plugin for monitoring
 -- PostgreSQL, see https://github.com/scoutapp/scout-plugins
--- then modified for better load with larger numbers of tables, as this approach
--- avoids resumming every aggregate twice for each table.  This is a marginal 
--- benefit since the PostgreSQL optimizer is very good at these things but the 
--- in a db under load with lots of tables, this seems like a good tradeoff
-WITH raw_stats AS (
-      SELECT sum(idx_tup_fetch) AS "rows_select_idx", 
-              sum(seq_tup_read) AS "rows_select_scan", 
-                 sum(n_tup_ins) AS "rows_insert", 
-                 sum(n_tup_upd) AS "rows_update",
-                 sum(n_tup_del) AS "rows_delete"
-        FROM pg_stat_all_tables)
+-- then modified  Takes delta since last run.
+
+WITH rawstats as (
+     SELECT sum(idx_tup_fetch) as r_select_idx,
+            sum(s.seq_tup_read) as r_select_seq,
+            sum(s.n_tup_ins) as r_insert,
+            sum(s.n_tup_upd) as r_update,
+            sum(s.n_tup_upd) as r_delete
+       FROM pg_stat_all_tables s
+),
+newstats AS (
+      UPDATE pg_monitoring_table_load_lastvals
+         SET rows_select_idx   = r.r_select_idx,
+             rows_select_scan  = r.r_select_seq,
+             rows_insert       = r.r_insert,
+             rows_update       = r.r_update,
+             rows_delete       = r.r_delete
+        FROM rawstats r
+   RETURNING *
+),
+old_vals AS (select * from pg_monitoring_table_load_lastvals),
+processed_vals AS (
+      select c.rows_select_idx - o.rows_select_idx as rows_select_idx,
+             c.rows_select_scan - o.rows_select_scan as rows_select_scan,
+             c.rows_insert - o.rows_insert as rows_insert,
+             c.rows_update - o.rows_update as rows_update,
+             c.rows_delete - o.rows_delete as rows_delete
+      FROM newstats c 
+cross join old_vals o
 )
 SELECT rows_select_idx, rows_select_scan, rows_insert, rows_update,
        rows_delete, 
        rows_select_idx + rows_select_scan + rows_insert + rows_update
        + rows_delete as rows_total
-  FROM raw_stats;
+  FROM processed_vals;
 $$;
 
 CREATE OR REPLACE FUNCTION pg_monitoring_load_across_databases()
 RETURNS TABLE (
-   numbackends int,
-   xact_commit bigint,
-   xact_rollback bigint,
-   xact_total bigint,
-   blks_read  bigint,
-   blks_hit   bigint
+   numbackends bigint,
+   xact_commit numeric,
+   xact_rollback numeric,
+   xact_total numeric,
+   blks_read numeric,
+   blks_hit numeric
 )
 LANGUAGE SQL AS
 $$
