65af4f1ef25af701260090594669e4d2bb31eeb6
Chris Travers <chris@efficito.com>
2014-04-21 22:40:28 -0700

    Changes in preparation for 0.2

--- pg_monitoring--0.1.sql
+++ pg_monitoring--0.1.sql
@@ -69,29 +69,44 @@ CREATE TABLE pg_monitoring_table_load_lastvals (
 
 insert into pg_monitoring_table_load_lastvals (rows_total) values (null);
 
-CREATE OR REPLACE FUNCTION pg_monitoring_load_across_tables()
+CREATE OR REPLACE FUNCTION pg_monitoring_raw_table_load()
 RETURNS SETOF pg_monitoring_table_load_lastvals
-LANGUAGE sql AS
+LANGUAGE SQL AS
 $$
 -- Copied and pasted from the MIT Licensed ScoutApp Plugin for monitoring
 -- PostgreSQL, see https://github.com/scoutapp/scout-plugins
--- then modified  Takes delta since last run.
+-- then modified somewhat.
 
-WITH rawstats as (
+WITH raw_stats AS (
      SELECT sum(idx_tup_fetch) as r_select_idx,
             sum(s.seq_tup_read) as r_select_seq,
             sum(s.n_tup_ins) as r_insert,
             sum(s.n_tup_upd) as r_update,
             sum(s.n_tup_upd) as r_delete
+            
        FROM pg_stat_all_tables s
+)
+SELECT r_select_idx, r_select_seq, r_insert, r_update, r_delete,
+       r_select_idx + r_select_seq + r_insert + r_update + r_delete
+  FROM raw_stats;
+$$;
+
+CREATE OR REPLACE FUNCTION pg_monitoring_load_across_tables()
+RETURNS SETOF pg_monitoring_table_load_lastvals
+LANGUAGE sql AS
+$$
+-- gives delta since last run.
+-- THIS IS NOT SAFE FOR CONCURRENT CALLS OBVIOUSLY
+WITH rawstats as (
+     select * from pg_monitoring_raw_table_load()
 ),
 newstats AS (
       UPDATE pg_monitoring_table_load_lastvals
-         SET rows_select_idx   = r.r_select_idx,
-             rows_select_scan  = r.r_select_seq,
-             rows_insert       = r.r_insert,
-             rows_update       = r.r_update,
-             rows_delete       = r.r_delete
+         SET rows_select_idx   = r.rows_select_idx,
+             rows_select_scan  = r.rows_select_scan,
+             rows_insert       = r.rows_insert,
+             rows_update       = r.rows_update,
+             rows_delete       = r.rows_delete
         FROM rawstats r
    RETURNING *
 ),
